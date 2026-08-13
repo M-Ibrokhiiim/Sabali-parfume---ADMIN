76dbd8208a91a6d2b9bc6b566f666108f692a0cf
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-08-13 19:11:37 +0500

    Updates for cloadflare

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -4,10 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  nitro: {
+    preset: 'cloudflare_module'
+  },
+
   css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+
+  modules: ['cloudflare']
 })
